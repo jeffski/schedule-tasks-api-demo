73d4244161065f3a06266e71ca4b92f56a854f90
Jeff Shillitto <jeff@shotstack.io>
2024-11-29 19:58:47 +1000

    Remove hello world boilerplate and import schedules module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SchedulesModule } from './app/modules/schedules/schedules.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [SchedulesModule],
 })
 export class AppModule {}
