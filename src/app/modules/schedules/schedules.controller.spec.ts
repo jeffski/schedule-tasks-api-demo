0a59cf123598b2417dc8621148cd2491eb7e22d3
Jeff Shillitto <jeff@shotstack.io>
2024-12-01 22:24:24 +1000

    Add controller tests for each method

--- src/app/modules/schedules/schedules.controller.spec.ts
+++ src/app/modules/schedules/schedules.controller.spec.ts
@@ -2,20 +2,72 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SchedulesController } from './schedules.controller';
 import { SchedulesService } from './schedules.service';
 import { PrismaService } from '../../../database/prisma/prisma.service';
+import { Prisma, PrismaClient, Schedule } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 describe('SchedulesController', () => {
   let controller: SchedulesController;
+  let prisma: DeepMockProxy<PrismaClient>;
+
+  const schedule: Schedule = {
+    id: 'e40fe564-2fb5-47b8-a01a-378d70c9b956',
+    account_id: 12345,
+    agent_id: 98765,
+    start_time: new Date(),
+    end_time: new Date(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SchedulesController],
       providers: [SchedulesService, PrismaService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     controller = module.get<SchedulesController>(SchedulesController);
+    prisma = module.get<DeepMockProxy<PrismaClient>>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a schedule', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = schedule;
+    const data: Prisma.ScheduleCreateInput = {
+      ...rest,
+    };
+
+    prisma.schedule.create.mockResolvedValueOnce(schedule);
+    expect(controller.create(data)).resolves.toBe(schedule);
+  });
+
+  it('should return a list of schedules', () => {
+    prisma.schedule.findMany.mockResolvedValueOnce([schedule]);
+    expect(controller.findAll()).resolves.toStrictEqual([schedule]);
+  });
+
+  it('should return a schedule by id', () => {
+    prisma.schedule.findUnique.mockResolvedValueOnce(schedule);
+    expect(controller.findOne('e40fe564-2fb5-47b8-a01a-378d70c9b956')).resolves.toBe(schedule);
+  });
+
+  it('should update a schedule by id', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = schedule;
+    const data: Prisma.ScheduleCreateInput = {
+      ...rest,
+    };
+
+    prisma.schedule.update.mockResolvedValueOnce(schedule);
+    expect(controller.update('e40fe564-2fb5-47b8-a01a-378d70c9b956', data)).resolves.toBe(schedule);
+  });
+
+  it('should return a delete a schedule', () => {
+    prisma.schedule.delete.mockResolvedValueOnce(schedule);
+    expect(controller.remove('e40fe564-2fb5-47b8-a01a-378d70c9b956')).resolves.toBe(schedule);
   });
 });
--- src/app/modules/tasks/tasks.controller.spec.ts
+++ src/app/modules/tasks/tasks.controller.spec.ts
@@ -2,20 +2,74 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { PrismaService } from '../../../database/prisma/prisma.service';
+import { Prisma, PrismaClient, Task, Type } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let prisma: DeepMockProxy<PrismaClient>;
+
+  const task: Task = {
+    id: '4ffa267c-2724-4c94-a0bb-8576a4e5a0d2',
+    account_id: 12345,
+    schedule_id: 'e40fe564-2fb5-47b8-a01a-378d70c9b956',
+    start_time: new Date(),
+    duration: 2,
+    type: Type.WORK,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
       providers: [TasksService, PrismaService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     controller = module.get<TasksController>(TasksController);
+    prisma = module.get<DeepMockProxy<PrismaClient>>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a task', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = task;
+    const data: Prisma.TaskCreateInput = {
+      ...rest,
+      schedule: undefined,
+    };
+
+    prisma.task.create.mockResolvedValueOnce(task);
+    expect(controller.create(data)).resolves.toBe(task);
+  });
+
+  it('should return a list of tasks', () => {
+    prisma.task.findMany.mockResolvedValueOnce([task]);
+    expect(controller.findAll()).resolves.toStrictEqual([task]);
+  });
+
+  it('should return a task by id', () => {
+    prisma.task.findUnique.mockResolvedValueOnce(task);
+    expect(controller.findOne('4ffa267c-2724-4c94-a0bb-8576a4e5a0d2')).resolves.toBe(task);
+  });
+
+  it('should update a task by id', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = task;
+    const data: Prisma.TaskCreateInput = {
+      ...rest,
+      schedule: undefined,
+    };
+    prisma.task.update.mockResolvedValueOnce(task);
+    expect(controller.update('4ffa267c-2724-4c94-a0bb-8576a4e5a0d2', data)).resolves.toBe(task);
+  });
+
+  it('should return a delete a task', () => {
+    prisma.task.delete.mockResolvedValueOnce(task);
+    expect(controller.remove('4ffa267c-2724-4c94-a0bb-8576a4e5a0d2')).resolves.toBe(task);
   });
 });
